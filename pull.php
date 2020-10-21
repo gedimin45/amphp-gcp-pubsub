503ad759869b0b225dbee820fce3a32aae8381ec
Gediminas Šedbaras <g.sedbaras@gmail.com>
2020-10-20 20:31:50 -0700

    removed unneeded $subscription variable

--- pull.php
+++ pull.php
@@ -21,13 +21,13 @@ if (!$subscription->exists()) {
     $subscription->create();
 }
 
-Loop::run(function() use ($subscription) {
+Loop::run(function() {
     $pullInProgress = false;
-    $messagesToAck = [];
+    $messagesToProcess = [];
 
     $pool = new DefaultPool;
 
-    $pull = function () use ($pool, &$pullInProgress, &$messagesToAck, $subscription) {
+    $pull = function () use ($pool, &$pullInProgress, &$messagesToProcess) {
         if ($pullInProgress) {
             echo "Pull already in progress\n";
             return;
@@ -44,23 +44,23 @@ Loop::run(function() use ($subscription) {
 
         foreach ($response['messages'] as $message) {
             yield $pool->enqueue(new Worker\CallableTask('processPulledMessage', [$message]));
-            $messagesToAck[] = $message;
+            $messagesToProcess[] = $message;
         }
     };
     Loop::repeat($msInterval = 1000, $pull);
 
-    Loop::repeat($msInterval = 1000, function () use ($pool, &$messagesToAck, $subscription) {
-        if ($messagesToAck === []) {
+    Loop::repeat($msInterval = 1000, function () use ($pool, &$messagesToProcess) {
+        if ($messagesToProcess === []) {
             return;
         }
 
-        echo "Scheduling an Ack for " . count($messagesToAck) . " messages\n";
+        echo "Scheduling an Ack for " . count($messagesToProcess) . " messages\n";
 
-        yield $pool->enqueue(new Worker\CallableTask('ackPubsubMessages', [$messagesToAck]));
-        $messagesToAck = [];
+        yield $pool->enqueue(new Worker\CallableTask('ackPubsubMessages', [$messagesToProcess]));
+        $messagesToProcess = [];
     });
 
-    Loop::repeat($msInterval = 1000, function () use ($pool, &$messagesToAck, $subscription) {
+    Loop::repeat($msInterval = 1000, function () use ($pool, &$messagesToProcess) {
         yield $pool->enqueue(new Worker\CallableTask('publishPubsubMessages', []));
     });
 });
